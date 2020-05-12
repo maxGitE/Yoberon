1096c78a52578c827322086c53d23e39973b6aee
Max <max.parkinn@gmail.com>
2020-05-12 12:20:06 +0200

    Added recoil to player weapon

---
+++ classes/Hitbox.js
@@ -0,0 +1,11 @@
+class Hitbox {
+    constructor(model) {
+        this._hitbox = undefined;
+        switch(model) {
+            case "player": break;
+            case "alien":
+                
+                break;
+        }
+    }
+}
